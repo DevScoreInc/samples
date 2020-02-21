94a01b9453dcc5c9bbf0028d2b8ee63e2aae2816
Khash Nejad <khash@leposha.com>
2020-02-21 15:07:18 -0800

    send calendar events to slack

--- google-calendar/index.js
+++ google-calendar/index.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 async function main() {
@@ -52,14 +51,104 @@ async function main() {
         return res;
     }
 
+    let post_slack_using_webhook = async (endpoint, text, blocks) => {
+        const options = {
+            "uri": endpoint,
+            "json": {
+                "text": text,
+                "blocks": blocks
+            }
+        };
+        let ret = await _context.requestAgent.post(options);
+        return ret;
+    }
+
+    let parse_calendar_events = (events) => {
+        if (events && events.items && events.items.length > 0) {
+            let taxt = `Hey, you have ${events.items.length} events planned for today`;
+            let items = events.items;
+            let blocks = [];
+            for (let i=0; i < items.length; i++) {
+                let current = items[i];
+                const link = current.htmlLink;
+                const summary = current.summary || 'no summary provided';
+                const description = current.description || 'no description provided';
+                const location = current.location || 'no location provided';
+                const start = current.start.dateTime || 'no start time provided';
+                const end = current.end.dateTime || 'no end time provided';
+                const attendees = current.attendees;
+                blocks.push({
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": summary
+                    }
+                });
+                blocks.push({
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": description
+                    }
+                });
+                blocks.push({
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": `Location: ${location}`
+                    }
+                });
+                blocks.push({
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": `Start: ${start}`
+                    }
+                });
+                blocks.push({
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": `End: ${end}`
+                    }
+                });
+                blocks.push({
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": `<${link}> \n :star: \n`
+                    },
+                    "accessory": {
+                        "type": "image",
+                        "image_url": "https://is5-ssl.mzstatic.com/image/thumb/Purple3/v4/d3/72/5c/d3725c8f-c642-5d69-1904-aa36e4297885/source/256x256bb.jpg",
+                        "alt_text": "Haunted hotel image"
+                    }
+                });
+                blocks.push({
+                    "type": "divider"
+                });
+            } //end for loop
+            return {'text': taxt, 'blocks': blocks};
+
+        } else {
+            return {'text': 'You do not have anything planned today', 'blocks': []};
+        }
+    }
+
+    const GENERAL_CHANNEL_WEBHOOK_ENDPOINT = "https://hooks.slack.com/services/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";
     const calendarId = "#YOUR_CALENDAR_ID#"; //It might be your email address
-    const timeMin = "2020-02-18T00:00:00-08:00";
-    const timeMax = "2020-02-20T23:59:59-08:00";
-    let tokens = await get_google_refresh_token();
-    let access_token = await refresh_google_token();
-    let events = await get_calendar_events(access_token, calendarId, timeMin, timeMax);
-    await _context.logger('info', {'events': events});
-    return {'message': events};
+    const TIMEZONE_UTC_OFFSET = -480; //America/Los_Angeles
+    const timeMin = _context.momentLib().utcOffset(TIMEZONE_UTC_OFFSET).startOf('day').format(); // "2020-02-18T00:00:00-08:00";
+    const timeMax = _context.momentLib().utcOffset(TIMEZONE_UTC_OFFSET).endOf('day').format(); //"2020-02-20T23:59:59-08:00";
+    let refreshToken = await get_google_refresh_token();
+    let access_token = await refresh_google_token(refreshToken);
+    let eventResponse = await get_calendar_events(access_token, calendarId, timeMin, timeMax);
+    // await _context.logger('info', {'eventResponse': eventResponse});
+    let parsed_events = parse_calendar_events(JSON.parse(eventResponse));
+    await _context.logger('info', {'events': parsed_events});
+    let ret = await post_slack_using_webhook(GENERAL_CHANNEL_WEBHOOK_ENDPOINT, parsed_events.text, parsed_events.blocks);
+    
+    return {'message': parsed_events};
 }
 main();
 
