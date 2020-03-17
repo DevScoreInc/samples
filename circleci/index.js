8cedca17cf493ff846a4f349100fea490108c9e0
Khash Nejad <khash@leposha.com>
2020-03-17 14:25:37 -0700

    add helper functions to get build data from DataStore

--- circleci/index.js
+++ circleci/index.js
@@ -5,6 +5,29 @@ async function main() {
 
     const baseURI = 'https://circleci.com/api/v2';
 
+    let getAllBuildsForTimePeriod = async (fromDate, toDate) => {
+        // This function returns all the build information that we send from CircleCI to DevScore
+        const variableName = 'circleci-builds';
+        const documentId = 'circleCI';
+        // API Docs: https://github.com/DevScoreInc/docs
+        const rows = await _context.dbLib.getTableRowsForTimePeriod(variableName, fromDate, toDate, documentId);
+        // Rows: {'data': data, 'status': 'success'}
+        return rows;
+    }
+
+    let getBuildsWithFilter = async (filterVariableName, filterVariableValue, filterOperator) => {
+        // This function returns all the build information that we send from CircleCI to DevScore
+        const variableName = 'circleci-builds';
+        const documentId = 'circleCI';
+        // const filterVariableName = 'reponame'; // It can be anythong you want
+        const shouldSort = false;
+        const limit = 10; // number of rows to return
+        // API Docs: https://github.com/DevScoreInc/docs
+        const rows = await _context.dbLib.getTableRowsWithFilterAndSort(variableName, filterVariableName, filterOperator, filterVariableValue, shouldSort, limit, documentId);
+        // Rows: {'data': data, 'status': 'success'}
+        return rows;
+    }
+
     let getProjectSlug = (version_control_system, username, repo_name) => {
         // version_control_system can be 'VCS', 'gh', or 'bb'
         // username can be org name or username
@@ -126,8 +149,19 @@ async function main() {
         return res;
     }
 
-    const token = await get_circleci_token();
-    const isValid = await validateCircleCIToken(token);
+    // const token = await get_circleci_token();
+    // const isValid = await validateCircleCIToken(token);
+
+
+    // Get Builds
+    // const TIMEZONE_UTC_OFFSET = -480; //America/Los_Angeles
+    // // Using Moment JS ... You can find docs here -> https://momentjs.com/docs/
+    // const fromDate = _context.momentLib().utcOffset(TIMEZONE_UTC_OFFSET).startOf('day').subtract(2, 'day').format(); 
+    // const toDate = _context.momentLib().utcOffset(TIMEZONE_UTC_OFFSET).endOf('day').format(); 
+    // const rows = await getAllBuildsForTimePeriod(fromDate, toDate);
+
+    const rows = await getBuildsWithFilter('reponame', 'samples', '==');
+    
     await _context.logger('debug', {'message': JSON.stringify(isValid)});
     return true;
 }
