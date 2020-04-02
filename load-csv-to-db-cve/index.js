854ca11d83ccd7929962ed13450373b053d87892
Khash Nejad <khash@leposha.com>
2020-04-02 11:57:27 -0700

    minor refactor

--- load-csv-to-db-cve/index.js
+++ load-csv-to-db-cve/index.js
@@ -25,12 +25,13 @@ async function main() {
         }
     }
             
-    let loadCSVFile = async (filePath) => {
-        return new Promise(async (resolve, reject) => { 
+    let loadCSVFile = (filePath) => {
+        if (!filePath || typeof filePath != "string") return null;
+        return new Promise((resolve, reject) => { 
             let csv = _context.fileLib.getCSVParserInstance();
             let stream = _context.fileLib.getFileReadStreamInstance(filePath);
             let rows = [];
-            // replace "/" in CSV file header with "--" 
+            // replace "/" in CSV file header with "--" because you cannot have "*~/[]" in the fields
             stream.pipe(csv.parse({
                 headers: headers => headers.map(h => h.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '--')),
             })).on('error', (error) => {
@@ -49,17 +50,17 @@ async function main() {
 
     //querty the db for any record that was published on a specific date. 2020-01-08 in this example. 
     let getDataForDate = async (varName, cveDate, documentId) => {
-        let ret = await _context.dbLib.getTableRowsWithFilterAndSort(varName, "Notes--Note--1--__text", "==", cveDate, false, 100, documentId);
+        const ret = await _context.dbLib.getTableRowsWithFilterAndSort(varName, "Notes--Note--1--__text", "==", cveDate, false, 100, documentId);
         return ret;
     }
     
     const filePath = await downloadCSVFileFromFileStorage('2020CVE-Data.csv');
-    let dataArray = await loadCSVFile(filePath);
+    const dataArray = await loadCSVFile(filePath);
     const varName = '2020CVE-Data';
     const documentId = '2020CVE-DataDoc';
     const cveDate = '2020-01-08';
-    let storeToDBResponse = await storeRowsToDB(varName, dataArray, documentId);
-    let cveDataforDate = await getDataForDate(varName, cveDate, documentId);
+    const storeToDBResponse = await storeRowsToDB(varName, dataArray, documentId);
+    const cveDataforDate = await getDataForDate(varName, cveDate, documentId);
     await _context.logger('debug', {'res': JSON.stringify(cveDataforDate)});
     return true;
 }
